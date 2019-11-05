14c72ee1fbd485ee6ee9f90cab66e96284bda807
unknown <shahbogdan@gmail.com>
2019-11-05 21:20:05 +0200

    add normal routes for jobber

--- app/api/controllers/jobber.js
+++ app/api/controllers/jobber.js
@@ -16,4 +16,58 @@ module.exports = {
                 res.json({ status: "success", message: "jobber added successfully!!!", data: null });
         });
     },
+    getByInn: function (req, res, next) {
+        console.log(req.body);
+        JobberSchema.find({inn:req.params.jobberInn}, function (err, jooberInfo) {
+            if (err) {
+                next(err);
+            } else {
+                res.json({ status: "success", message: "Movie found!!!", data: { movies: jooberInfo } });
+            }
+        });
+    },
+    getByPhone: function (req, res, next) {
+        console.log(req.body);
+        JobberSchema.find({phone:req.params.jobberPhone}, function (err, jooberInfo) {
+            if (err) {
+                next(err);
+            } else {
+                res.json({ status: "success", message: "Movie found!!!", data: { movies: jooberInfo } });
+            }
+        });
+    },
+    getById: function (req, res, next) {
+        console.log(req.body);
+        JobberSchema.findById(req.params.jobberId, function (err, movieInfo) {
+            if (err) {
+                next(err);
+            } else {
+                res.json({ status: "success", message: "jooberId found!!!", data: { movies: movieInfo } });
+            }
+        });
+    },
+    getAll: function (req, res, next) {
+        let joobersList = [];
+        JobberSchema.find({}, function (err, joobers) {
+            if (err) {
+                next(err);
+            } else {
+
+                for (let joober of joobers) {
+                    joobersList.push({
+                        joober_id: joober._id,
+                        first_name: joober.first_name, 
+                        last_name: joober.last_name , 
+                        second_name: joober.second_name , 
+                        inn: joober.inn , 
+                        birthday: joober.birthday , 
+                        phone: joober.phone
+                    });
+                }
+
+                res.json({ status: "success", message: "Joobers list found!", data: { joobers: joobersList } });
+            }
+        })
+    }
 }
+
--- routes/jobber.js
+++ routes/jobber.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const jobberController = require('../app/api/controllers/jobber');
+router.get('/', jobberController.getAll);
+router.get('/:jobberInn', jobberController.getByInn);
+router.get('/id/:jobberId', jobberController.getById);
+
 router.post('/', jobberController.create);
+
 module.exports = router;
